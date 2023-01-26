f47d312880a4b1256d1015dd3260032862831c6a
HamidReza <delshad.swdn@gmail.com>
2023-01-27 00:21:53 +0100

    add hero image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,10 +9,18 @@ const Hero = () => {
           <br />
           <span>Delshad</span>
         </h1>
+        <p>
+          I am a Graphic Designer, UI/UX artist and Junior web developer.
+          Previously I had Bachelor's degree in Computer Science from Iran. I
+          have 18 years of work experience in Graphic Design and many years in
+          UI/ UX and tools like Adobe Photoshop, Illustrator, InDesign,
+          Lightroom, CorelDraw, Freehand, Figma and Adobe XD and had worked
+          within teams as well as on individual projects. Also, I can do
+          programming with HTLM5, CSS, Javascript, React.Js, and Python for Web
+          Development.
+        </p>
       </div>
       <div className="hero-right">
-        <div>right-left</div>
-        <div>right-right</div>
       </div>
     </div>
   );
